346172fde64014e793cd7e72370f10240b396f7f
ThunderIW <43197270+ThunderIW@users.noreply.github.com>
2025-03-15 12:59:08 -0700

    added game_over_switch
    When the player loses their lives, the game freezes and after 1.5 seconds returns to the main menu

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
 
 public class GameManager : SingletonMonoBehavior<GameManager>
 {
     [SerializeField] private int maxLives = 3;
     [SerializeField] private Ball ball;
     [SerializeField] private Transform bricksContainer;
+    [SerializeField] private float score=0;
+    [SerializeField] private float lives = 0;
 
     private int currentBrickCount;
     private int totalBrickCount;
@@ -33,15 +37,51 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         // implement particle effect here
         // add camera shake here
         currentBrickCount--;
+        score++;
         Debug.Log($"Destroyed Brick at {position}, {currentBrickCount}/{totalBrickCount} remaining");
-        if(currentBrickCount == 0) SceneHandler.Instance.LoadNextScene();
+        Debug.Log($"Current Score: {score}");
+        if (currentBrickCount <= 0) {
+            score = 0;
+            SceneHandler.Instance.LoadNextScene(); 
+            }
     }
 
     public void KillBall()
     {
         maxLives--;
+        Debug.Log($"Current Lives: {maxLives}");
+        if (maxLives <= 0)
+        {
+            StartCoroutine(GameOverRoutine());
+            
+        }
+        else {
+            ball.ResetBall();
+        }
         // update lives on HUD here
         // game over UI if maxLives < 0, then exit to main menu after delay
-        ball.ResetBall();
+       
     }
+
+    private IEnumerator GameOverRoutine()
+    {
+        Debug.Log("GameOverRoutine started.");
+
+        Time.timeScale = 0f;
+        yield return new WaitForSecondsRealtime(1.5f);
+        Time.timeScale = 1f;
+
+        Debug.Log("Coroutine finished waiting, trying to switch scenes.");
+
+        if (SceneHandler.Instance != null)
+        {
+            SceneHandler.Instance.LoadMenuScene();
+            Debug.Log("LoadMenuScene was called.");
+        }
+        else
+        {
+            Debug.LogError("SceneHandler.Instance is null. Scene transition failed.");
+        }
+    }
+
 }
